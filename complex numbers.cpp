0a86c0fc3a333376f93f6bb7b4949bd0fdbd1d7c
Menna Elminshawy <91240304+menna03@users.noreply.github.com>
2023-03-17 23:07:55 +0200

    Add files via upload

---
+++ complex numbers.cpp
@@ -0,0 +1,61 @@
+// problem 1
+/* Menna Osama Elminshawy (20217011)
+Mariam Ashraf Ezzat (20216140)
+s4 */
+#include <iostream>
+using namespace std;
+struct complex{
+    double real1;
+    double real2;
+    double imaginary1 ;
+    double imaginary2 ;
+    char operation ;
+};
+int add(complex& c )
+{
+    float realsum,imaginarysum;
+    realsum=c.real1+c.real2;
+    imaginarysum=c.imaginary1+c.imaginary2;
+    cout<<realsum<<"+"<<imaginarysum<<'i';
+}
+int subtract(complex& c)
+{
+    float realsub,imaginarysub;
+    realsub=c.real1-c.real2;
+    imaginarysub=c.imaginary1-c.imaginary2;
+    cout<<realsub<<"+"<<imaginarysub<<'i';
+}
+int multiply(complex& c)
+{    float realmul,imaginarymul;
+        realmul=(c.real1*c.real2)+((c.imaginary1*c.imaginary2)*(-1));
+
+    imaginarymul=+(c.real1 * c.imaginary2)+(c.real2*c.imaginary1);
+    cout<<realmul<<"+"<<imaginarymul<<'i';
+}
+int main()
+{
+    complex c ;
+    char i;
+    cout<<" pleas enter first complex numbers in the form x + y i with your desired operations :\n" ;
+    cin>> c.real1>>c.imaginary1>>i;
+    cout<<"operation";
+    cin>>c.operation;
+    cout<<" pleas enter second complex numbers in the form x + y i with your desired operations :\n" ;
+    cin>>c.real2>>c.imaginary2>>i;
+if(c.operation=='+')
+{
+    add( c );
+}
+else if ( c.operation=='-')
+{
+    subtract(c);
+}
+else if (c.operation=='x')
+{
+    multiply(c);
+}
+else
+{
+    cout<<"no existing operation \n ";
+}
+}
